c5b62a5cab05d595f2948798b3103bd99a93bf22
https://github.com/RabDino-Phulpoto <rabdinophulpoto@gmail.com>
2025-07-02 15:04:04 +0500

    Initial commit with frontend and backend

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import cors from 'cors'
+import 'dotenv/config'
+import express from 'express'
+
+const PORT =process.env.PORT || 4000
+const app=express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>res.send("API Working"))
+
+app.listen(PORT,()=>console.log('Server running on port '+PORT));
