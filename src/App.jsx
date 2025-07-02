9d1daa7e2e4165c318284fc278c6883691dbce68
https://github.com/RabDino-Phulpoto <rabdinophulpoto@gmail.com>
2025-07-02 10:57:32 +0500

    i have coded the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
+import Login from './components/Login'
 import Navbar from './components/navbar'
 import BuyCredit from "./pages/BuyCredit"
 import Home from "./pages/Home"
@@ -9,13 +10,14 @@ const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
 <Navbar/>
+<Login/>
       <Routes>
               <Route path='/' element={ <Home/>}/>
               <Route path='/result' element={ <Result/> }/>
               <Route path='/buy' element={ <BuyCredit/> }/>
 
       </Routes>
-      <Footer/>
+      <Footer/> 
 
     </div>
   )
